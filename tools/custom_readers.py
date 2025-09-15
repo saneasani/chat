20e87e1d5327a5bfd68b30be4c2e245f56249b68
saneasani <150718618+saneasani@users.noreply.github.com>
2025-09-15 13:35:36 +0300

    Add files via upload

---
+++ tools/custom_readers.py
@@ -0,0 +1,41 @@
+import sys
+import asyncio
+from collections import deque
+from asyncio import StreamReader
+from tools.manage_shell import move_back_one_char, clear_line
+
+
+async def create_stdin_reader() -> StreamReader:
+    stream_reader = asyncio.StreamReader()
+    protocol = asyncio.StreamReaderProtocol(stream_reader)
+    loop = asyncio.get_running_loop()
+    await loop.connect_read_pipe(lambda: protocol, sys.stdin)
+    return stream_reader
+
+
+
+async def read_line(stdin_reader: StreamReader) -> str:
+    def erase_last_char():
+        move_back_one_char()
+        sys.stdout.write(' ')
+        move_back_one_char()
+
+    delete_char = b'\x7f'
+    input_buffer = deque()
+
+    while (input_char := await stdin_reader.read(1)) != b'\n':
+        if input_char == b'\x1b':
+            continue
+
+        if input_char == delete_char:
+            if len(input_buffer) > 0:
+                input_buffer.pop()
+                erase_last_char()
+                sys.stdout.flush()
+        else:
+            input_buffer.append(input_char)
+            sys.stdout.write(input_char.decode())
+            sys.stdout.flush()
+            
+    clear_line()
+    return b''.join(input_buffer).decode()
